e5f89a4788849bb12f91882580e1699284bef61f
Michael Medford <MichaelMedford@berkeley.edu>
2020-04-03 11:36:10 -0700

    working version of GalaxyParameters using dummy data

--- src/Galaxia.cpp
+++ src/Galaxia.cpp
@@ -10,7 +10,8 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include "StellarPopulation.h"
-#include"Parameters.h"
+#include "Parameters.h"
+#include "GalaxyParameters.h"
 #include "Timer.h"
 #include "ebfvector.hpp"
 //#include "GInterpolator.h"
@@ -376,8 +377,15 @@ int main(int argc, char **argv)
 
 	//--------------------
 	Parameters All;
-	All.setFromArguments(argc, argv);
-	//	All.print();
+//	All.setFromArguments(argc, argv);
+//    All.print();
+
+	GalaxyParameters GalaxyParams;
+	GalaxyParams.print();
+	cout << " -------- " << endl;
+	GalaxyParams.setFromParameterFile(argv[1]);
+    return 1;
+
 	nrRan = Ran(All.seed+4);
 	nrGauss = Normaldev(0.0, 1.0, All.seed);
 	Interp vcirc(All.inputDir + "Model/vcirc.dat");
---
+++ src/GalaxyParameters.h
@@ -0,0 +1,35 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Author: Sanjib Sharma                                                     *
+ * Copyright (c) 2012 Sanjib Sharma                                          *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Galaxia. The full Galaxia copyright notice, including*
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and COPYRIGHT, which can be found at the root           *
+ * of the source code distribution tree.                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+#ifndef GALAXYPARAMETERS_H_
+#define GALAXYPARAMETERS_H_
+
+#include "Parameters.h"
+
+class GalaxyParameters
+{
+public:
+	GalaxyParameters();
+	~GalaxyParameters();
+	void setFromParameterFile(const string fname);
+	void print( );
+	void checkCompilation( );
+	string GalaxiaData;
+	double mass;
+	int ncomp;
+	std::vector<ParMem>  par_list;
+};
+
+string version();
+
+#endif /*GALAXYPARAMETERS_H_*/
